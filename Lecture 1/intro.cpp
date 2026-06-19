b88cda5492b067bbc4b63dbe43145cad31276212
Rayyan Rasool Mir <rayyanrasoolmir@gmail.com>
2026-06-19 18:58:45 +0530

    Starting

---
+++ Lecture 1/intro.cpp
@@ -0,0 +1,18 @@
+/*
+Find the area of rectangle using lb
+constraints:
+1<=l<=10^9
+1<=b<=10^9
+
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long int l, b; //due to constraints we can't just take int, we need higher storage datatypes like long long int;
+    cin >>l >> b;
+
+    cout<< l*b<<endl;
+
+}

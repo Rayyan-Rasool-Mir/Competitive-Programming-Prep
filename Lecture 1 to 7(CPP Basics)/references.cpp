1c97edec0b2246a37fe9f0b45371b5370bb6d26c
Rayyan Rasool Mir <rayyanrasoolmir@gmail.com>
2026-06-27 23:43:41 +0530

    References in cpp

---
+++ Lecture 1 to 7(CPP Basics)/references.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    int &b = a;
+
+    cout << a << " " << b << endl;
+
+    b = 20;
+
+    cout << a << " " << b << endl;
+
+    return 0;
+}

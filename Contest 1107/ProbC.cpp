f7aa0749845ebf1a26fd3c629f027ef5604eb0b1
Rayyan Rasool Mir <rayyanrasoolmir@gmail.com>
2026-07-01 23:42:17 +0530

    Prob C

---
+++ Contest 1107/ProbC.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        string s;
+        cin >> s;
+
+        int count = 1;
+        for (int i = 1; i < n; i++)
+        {
+            if (s[i] != s[i - 1])
+                count++;
+        }
+
+        if (count == 2)
+        {
+            cout << 2 << endl;
+        }
+        else
+        {
+            cout << 1 << endl;
+        }
+    }
+
+    return 0;
+}

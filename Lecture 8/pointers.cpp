c7f7a4376a56509e80adc6d82d0976b1c4ff17bb
Rayyan Rasool Mir <rayyanrasoolmir@gmail.com>
2026-07-07 21:36:02 +0530

    Pointers start

---
+++ Lecture 8/pointers.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <deque>
+#include <numeric>
+#include <cmath>
+#include <climits>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+
+    int *ptr = &a;
+
+    *ptr = 20;
+
+    cout<< *ptr<<endl;
+    return 0;
+}

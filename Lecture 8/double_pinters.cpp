9c11f2a5e7aca7ac73a6106fa840d38ff020e10a
Rayyan Rasool Mir <rayyanrasoolmir@gmail.com>
2026-07-09 23:24:37 +0530

    Starting with double pointers

---
+++ Lecture 8/double_pinters.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+
+    int* p = &a;
+
+    int** pp = &p;
+
+    cout << a << endl;
+    cout << p << endl;
+    cout << pp << endl;
+}
